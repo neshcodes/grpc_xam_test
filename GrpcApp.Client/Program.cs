32a3ae38977f22510eb8dcade81986e4e857412d
neshcodes <neshcodes@gmail.com>
2020-02-28 21:12:31 +0800

    commit

--- GrpcApp.Client/Program.cs
+++ GrpcApp.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Net.Client;
@@ -7,8 +8,52 @@ using ProtoBuf.Grpc.Client;
 
 namespace GrpcApp.Client
 {
+    public static class GrpcExtensions
+    {
+        public static Task<TResponse> Execute<TRequest, TResponse>(this GrpcChannel channel, TRequest request, string serviceName, string methodName,
+            CallOptions options = default, string? host = null)
+            where TRequest : class
+            where TResponse : class
+            => Execute<TRequest, TResponse>(channel.CreateCallInvoker(), request, serviceName, methodName, options, host);
+
+        public static async Task<TResponse> Execute<TRequest, TResponse>(this CallInvoker invoker, TRequest request, string serviceName, string methodName,
+            CallOptions options = default, string? host = null)
+            where TRequest : class
+            where TResponse : class
+        {
+            var method = new Method<TRequest, TResponse>(MethodType.Unary, serviceName, methodName,
+                CustomMarshaller<TRequest>.Instance, CustomMarshaller<TResponse>.Instance);
+            using (var auc = invoker.AsyncUnaryCall(method, host, options, request))
+            {
+                return await auc.ResponseAsync;
+            }
+        }
+
+        class CustomMarshaller<T> : Marshaller<T>
+        {
+            public static readonly CustomMarshaller<T> Instance = new CustomMarshaller<T>();
+            private CustomMarshaller() : base(Serialize, Deserialize) { }
+
+            private static T Deserialize(byte[] payload)
+            {
+                using (var ms = new MemoryStream(payload))
+                {
+                    return ProtoBuf.Serializer.Deserialize<T>(ms);
+                }
+            }
+            private static byte[] Serialize(T payload)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    ProtoBuf.Serializer.Serialize<T>(ms, payload);
+                    return ms.ToArray();
+                }
+            }
+        }
+    }
     class Program
     {
+
         static async Task Main()
         {
             try
@@ -16,9 +61,9 @@ namespace GrpcApp.Client
                 GrpcClientFactory.AllowUnencryptedHttp2 = true;
                 using (var channel = GrpcChannel.ForAddress("http://localhost:50001"))
                 {
-                    var login_rpc = channel.CreateGrpcService<IGreeterService>();
-                    var result = await login_rpc.SayHello(new HelloRequest() { Name = Guid.NewGuid().ToString() });
-                    Console.WriteLine(result.Text);
+                    HelloRequest request = new HelloRequest() { Name = Guid.NewGuid().ToString() };
+                    var response = await channel.Execute<HelloRequest, HelloReply>(request, nameof(IGreeterService), nameof(IGreeterService.SayHello));
+                    Console.WriteLine(response.Text);
                 }
             }
             catch (Exception ex)
--- GrpcApp.Common/IGreeterService.cs
+++ GrpcApp.Common/IGreeterService.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Threading.Tasks;
+using ProtoBuf;
 
 namespace GrpcApp.Common
 {
-    [ServiceContract]
+    [ServiceContract, ProtoContract]
     public interface IGreeterService
     {
         ValueTask<HelloReply> SayHello(HelloRequest request);
     }
 
-    [DataContract]
+    [DataContract, ProtoContract]
     public class HelloReply
     {
-        [DataMember(Order = 1)]
+        [DataMember(Order = 1), ProtoMember(1)]
         public string Text { get; set; }
     }
 
-    [DataContract]
+    [DataContract, ProtoContract]
     public class HelloRequest
     {
-        [DataMember(Order = 1)]
+        [DataMember(Order = 1), ProtoMember(1)]
         public string Name { get; set; }
     }
 }
